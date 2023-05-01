fa91499b1d12193a538bc0257d04f00252b4c02e
Joshua Oyeleke <oyelekeoluwasayo@gmail.com>
2023-05-01 13:56:59 +0100

    renamed mongodb file

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 
 const dotenv = require("dotenv");
-const connectDatabase = require("./config/mongodb");
-
+const connectDatabase = require("./config/database");
 const route_handler = require("./routes/_config");
 
 /** 3rd Party Middlewares */
